dc551c8cdc2140e6d22f369b0344581353a495b7
Mikhail <mikhail.a@praxiscashier.com>
2020-11-02 01:00:55 +0200

    User statuses changed

--- common/models/User.php
+++ common/models/User.php
@@ -24,9 +24,9 @@ use yii\web\IdentityInterface;
  */
 class User extends ActiveRecord implements IdentityInterface
 {
-    const STATUS_DELETED = 0;
-    const STATUS_INACTIVE = 9;
-    const STATUS_ACTIVE = 10;
+    const STATUS_INACTIVE = 0;
+    const STATUS_ACTIVE = 1;
+    const STATUS_DELETED = 2;
 
 
     /**
