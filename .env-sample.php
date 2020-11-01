722ce55023a3715a405dc7245292fdbf82dbe3f4
Mikhail <mikhail.a@praxiscashier.com>
2020-11-01 19:41:27 +0200

    init commit

---
+++ .env-sample.php
@@ -0,0 +1,9 @@
+# Environment configs
+YII_DEBUG=true
+YII_ENV=dev
+
+# Mysql config
+DB_HOST=mysql
+DB_NAME=yii2advanced
+DB_USERNAME=****
+DB_PASSWORD=****
---
+++ environments/dev/common/config/test-local.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'dsn' => 'mysql:host=127.0.0.1;dbname=yii2advanced_test',
+        ],
+    ],
+];
